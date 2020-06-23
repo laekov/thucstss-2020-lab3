d5be0f9051a3fd6969501aafd58c4d857447039d
Rick Ho <laekov.h@gmail.com>
2020-06-23 20:26:39 +0800

    fix minor data

--- src/main.py
+++ src/main.py
@@ -10,7 +10,7 @@ from models import BaseCNNModel, AdvancedModel
 parser = argparse.ArgumentParser(description='Captcha Model Training and Testing')
 parser.add_argument('--data-path', '-i', type=str, default='../data/',
         help='path to input data')
-parser.add_argument('--epochs', '-n', type=int, default=32,
+parser.add_argument('--epochs', '-n', type=int, default=16,
         help='number of epochs to train')
 parser.add_argument('--model', default='base',
         help='Which model to run [base|advanced]')
@@ -36,10 +36,10 @@ def main():
     if len(gpus) == 0:
         print('Warning: No GPUs present on this node, using CPU')
     else:
-        # GPU memory is limited to 8 GBs here
+        # GPU memory is limited to 3 GBs here
         # Remove the following line if you want more GPU memory
         tf.config.experimental.set_virtual_device_configuration(gpus[0],
-            [tf.config.experimental.VirtualDeviceConfiguration(memory_limit=8192)])
+            [tf.config.experimental.VirtualDeviceConfiguration(memory_limit=3072)])
 
     x_train, y_train, x_test, y_test = load_data(args.data_path, 0.9)
     model = AdvancedModel() if args.model == 'advanced' else BaseCNNModel()
